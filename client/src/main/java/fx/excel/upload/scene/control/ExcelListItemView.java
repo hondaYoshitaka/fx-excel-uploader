c1ca5fdf0adf1ec37c64064d3dcbdd086423efe9
honda <yhonda1001@gmail.com>
2015-01-25 20:47:29 +0900

    リフレッシュ時に以前のリストアイテムがラベルとして残ってしまっていたので修正しました。

--- client/src/main/java/fx/excel/upload/scene/control/ExcelListItemView.java
+++ client/src/main/java/fx/excel/upload/scene/control/ExcelListItemView.java
@@ -1,17 +1,18 @@
 package fx.excel.upload.scene.control;
 
+import javafx.scene.control.ListCell;
 import fx.excel.upload.model.ExcelListModel;
 
-import javafx.scene.control.ListCell;
-
 public class ExcelListItemView extends ListCell<ExcelListModel> {
-	
+
+	/**
+	 * modelに変更があった時の処理
+	 */
 	@Override
 	protected void updateItem(ExcelListModel model, boolean empty) {
 		super.updateItem(model, empty);
-		
-		if (!empty) {
-			setText(model.fileName);
-		}
+
+		// nullで更新しないと以前のテキストが残ってしまう
+		this.setText(empty ? null : model.fileName);
 	}
 }
