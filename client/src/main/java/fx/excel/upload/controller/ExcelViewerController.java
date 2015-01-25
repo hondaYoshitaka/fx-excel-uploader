9ef443fc28841743e90602cdec518c9ad55c1c93
honda <yhonda1001@gmail.com>
2015-01-25 20:46:53 +0900

    e.printStackTrace();が不要だったので削除しました。

--- client/src/main/java/fx/excel/upload/controller/ExcelViewerController.java
+++ client/src/main/java/fx/excel/upload/controller/ExcelViewerController.java
@@ -7,12 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
-import fx.excel.upload.model.ExcelListModel;
-import fx.excel.upload.scene.control.ExcelListItemView;
-import fx.excel.upload.scene.control.SpreadSheetView;
-import fx.excel.upload.scene.control.SpreadSheetView.SpreadSheetProperty;
-import fx.excel.upload.service.ExcelService;
-
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -27,22 +21,27 @@ import javafx.scene.control.MultipleSelectionModel;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.util.Callback;
+import fx.excel.upload.model.ExcelListModel;
+import fx.excel.upload.scene.control.ExcelListItemView;
+import fx.excel.upload.scene.control.SpreadSheetView;
+import fx.excel.upload.scene.control.SpreadSheetView.SpreadSheetProperty;
+import fx.excel.upload.service.ExcelService;
 
 public class ExcelViewerController implements Initializable {
-	
+
 	@FXML
 	public Button fileListRefreshBtn;
-	
+
 	@FXML
 	public ListView<ExcelListModel> excelListView;
-	
+
 	@FXML
 	public SpreadSheetView excelDetail;
-	
+
 	private ObservableList<ExcelListModel> excelListModelList;
-	
+
 	private ExcelService excelService = new ExcelService();
-	
+
 	@Override
 	public void initialize(URL paramURL, ResourceBundle paramResourceBundle) {
 		List<Map<String, Object>> excelList = null;
@@ -51,23 +50,23 @@ public class ExcelViewerController implements Initializable {
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		excelListModelList = FXCollections.observableArrayList();
 		for (Map<String, Object> map : excelList) {
 			excelListModelList.add(createExcelListModel(map));
 		}
 		this.excelListView.setItems(excelListModelList);
 		this.excelListView.setCellFactory(new Callback<ListView<ExcelListModel>, ListCell<ExcelListModel>>() {
-			
+
 			@Override
 			public ListCell<ExcelListModel> call(ListView<ExcelListModel> arg0) {
 				return new ExcelListItemView();
 			}
 		});
-		
+
 		MultipleSelectionModel<ExcelListModel> listSelectionModel = this.excelListView.getSelectionModel();
 		listSelectionModel.selectedItemProperty().addListener(new ChangeListener<ExcelListModel>() {
-			
+
 			@Override
 			public void changed(ObservableValue<? extends ExcelListModel> arg0, ExcelListModel arg1, ExcelListModel arg2) {
 				if (arg2 == null || arg2.excelFileId == null) {
@@ -87,24 +86,24 @@ public class ExcelViewerController implements Initializable {
 					maxSize = cels.size();
 				}
 				excelDetail.createColumns(maxSize);
-				
+
 				ObservableList<SpreadSheetProperty> detail = FXCollections.observableArrayList();
 				for (int rowNum = 0; rowNum < excelDetailData.size(); rowNum++) {
 					List<String> rowData = excelDetailData.get(rowNum);
-					
+
 					detail.add(new SpreadSheetProperty(rowNum, rowData));
 				}
 				excelDetail.setItems(detail);
 			}
 		});
 	}
-	
+
 	@FXML
 	public void handleFileChoose(ActionEvent event) {
 		FileChooser fileChooser = new FileChooser();
-		
+
 		fileChooser.getExtensionFilters().add(new ExtensionFilter("Excelファイル", "*.xls", "*.xlsx"));
-		
+
 		File excel = fileChooser.showOpenDialog(null);
 		if (excel == null) {
 			return;
@@ -112,12 +111,11 @@ public class ExcelViewerController implements Initializable {
 		try {
 			excelService.insert(excel);
 		} catch (Exception e) {
-			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
 		handleListRefresh(event);
 	}
-	
+
 	@FXML
 	public void handleListRefresh(ActionEvent event) {
 		List<Map<String, Object>> excelList = null;
@@ -127,13 +125,13 @@ public class ExcelViewerController implements Initializable {
 			throw new RuntimeException(e);
 		}
 		excelListModelList.clear();
-		
+
 		for (Map<String, Object> map : excelList) {
 			excelListModelList.add(createExcelListModel(map));
 		}
 		event.consume();
 	}
-	
+
 	/**
 	 * mapから Modelエンティティを作成します。
 	 *
@@ -142,10 +140,10 @@ public class ExcelViewerController implements Initializable {
 	 */
 	private static ExcelListModel createExcelListModel(Map<String, Object> map) {
 		ExcelListModel model = new ExcelListModel();
-		
+
 		model.fileName = (String) map.get("fileName");
 		model.excelFileId = (String) map.get("excelFileId");
-		
+
 		return model;
 	}
 }
