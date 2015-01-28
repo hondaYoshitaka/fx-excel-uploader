6d969737b4a68aceb40c1c2ce12e200610ee9170
honda yoshitaka <yhonda1001@gamil.com>
2015-01-28 18:31:07 +0900

    サポートされていないexcel関数を使用した際の処理を追加しました。

--- web/src/main/java/fx/excel/upload/logic/ExcelLogic.java
+++ web/src/main/java/fx/excel/upload/logic/ExcelLogic.java
@@ -8,6 +8,7 @@ import net.arnx.jsonic.JSON;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.formula.eval.NotImplementedException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.FormulaEvaluator;
@@ -18,16 +19,16 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.seasar.framework.util.tiger.CollectionsUtil;
 
 public class ExcelLogic {
-
+	
 	public String fetchFileData(File file, int sheetAt) throws InvalidFormatException, IOException {
 		Workbook workbook = null;
-
+		
 		try {
 			workbook = WorkbookFactory.create(file);
-
+			
 			Sheet sheet = workbook.getSheetAt(sheetAt);
 			sheet.setForceFormulaRecalculation(true);
-
+			
 			List<List<String>> resultList = CollectionsUtil.newArrayList();
 			for (Row row : sheet) {
 				List<String> rowList = CollectionsUtil.newArrayList();
@@ -37,50 +38,62 @@ public class ExcelLogic {
 				resultList.add(rowList);
 			}
 			return JSON.encode(resultList);
-
+			
 		} finally {
 			IOUtils.closeQuietly(workbook);
 		}
 	}
-
+	
 	/**
 	 * セルの値を文字列として返す
-	 * 
+	 *
 	 * @param cell
 	 * @return
 	 */
 	private static String getCellValue(Cell cell) {
 		switch (cell.getCellType()) {
-		case Cell.CELL_TYPE_BLANK:
-			// TODO 結合セルの場合どう扱うか？
-			return "";
-		case Cell.CELL_TYPE_BOOLEAN:
-			return Boolean.toString(cell.getBooleanCellValue());
-		case Cell.CELL_TYPE_NUMERIC:
-			return Double.toString(cell.getNumericCellValue());
-		case Cell.CELL_TYPE_STRING:
-			return cell.getStringCellValue();
-		case Cell.CELL_TYPE_FORMULA:
-			return getStringFormulaValue(cell);
-		case Cell.CELL_TYPE_ERROR:
-			return "#N/A";
+			case Cell.CELL_TYPE_BLANK:
+				// TODO 結合セルの場合どう扱うか？
+				return "";
+			case Cell.CELL_TYPE_BOOLEAN:
+				return Boolean.toString(cell.getBooleanCellValue());
+			case Cell.CELL_TYPE_NUMERIC:
+				return Double.toString(cell.getNumericCellValue());
+			case Cell.CELL_TYPE_STRING:
+				return cell.getStringCellValue();
+			case Cell.CELL_TYPE_FORMULA:
+				return getStringFormulaValue(cell);
+			case Cell.CELL_TYPE_ERROR:
+				return "#N/A";
 		}
 		return "";
 	}
-
+	
 	/**
 	 * 数式を再計算し、文字列として返す
-	 * 
+	 *
+	 * <p>
+	 * ◇使用可能な関数について
+	 * </p>
+	 * <ul>
+	 * <li>http://poi.apache.org/spreadsheet/eval-devguide.html</li>
+	 * <li>{@link org.apache.poi.ss.formula.eval.FunctionEval}</li>
+	 * </ul>
+	 *
 	 * @param cell
 	 * @return
 	 */
 	private static String getStringFormulaValue(Cell cell) {
-		Workbook book = cell.getSheet().getWorkbook();
-
-		CreationHelper helper = book.getCreationHelper();
-		FormulaEvaluator evaluator = helper.createFormulaEvaluator();
-		Cell evalCell = evaluator.evaluateInCell(cell);
-
-		return getCellValue(evalCell);
+		try {
+			Workbook book = cell.getSheet().getWorkbook();
+			
+			CreationHelper helper = book.getCreationHelper();
+			FormulaEvaluator evaluator = helper.createFormulaEvaluator();
+			Cell evalCell = evaluator.evaluateInCell(cell);
+			
+			return getCellValue(evalCell);
+		} catch (NotImplementedException e) {
+			return "#UNSUPPORT";
+		}
 	}
 }
