e894e859c9876a966c3bfcf45cf332989c8e9afc
honda <yhonda1001@gmail.com>
2015-02-05 11:32:55 +0900

    擬似的なExcelに項目の型と値をセットして、描画時に値を取り出せるようにしました。

--- client/src/main/java/fx/excel/upload/scene/control/SpreadSheetView.java
+++ client/src/main/java/fx/excel/upload/scene/control/SpreadSheetView.java
@@ -1,16 +1,8 @@
 package fx.excel.upload.scene.control;
 
+import java.math.BigDecimal;
 import java.util.List;
-
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import fx.excel.upload.scene.control.SpreadSheetView.SpreadSheetRowData;
-import fx.excel.upload.util.ExcelUtil;
+import java.util.Map;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -27,11 +19,21 @@ import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.util.Callback;
 import javafx.util.converter.DefaultStringConverter;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import fx.excel.upload.scene.control.SpreadSheetView.SpreadSheetRowData;
+import fx.excel.upload.util.ExcelUtil;
+
 public class SpreadSheetView extends TableView<SpreadSheetRowData> {
-	
+
 	/** 数式計算のための擬似的なExcelシート */
 	private Sheet sheet;
-	
+
 	@SuppressWarnings("resource")
 	public void init(String extension) {
 		ObservableList<TableColumn<SpreadSheetRowData, ?>> columns = this.getColumns();
@@ -52,7 +54,7 @@ public class SpreadSheetView extends TableView<SpreadSheetRowData> {
 		sheet = workbook.createSheet();
 		sheet.setForceFormulaRecalculation(true);
 	}
-	
+
 	/**
 	 * ヘッダー行とカラム設定を作成します。
 	 *
@@ -64,35 +66,38 @@ public class SpreadSheetView extends TableView<SpreadSheetRowData> {
 		rowHeader.setId("excel-row-header");
 		rowHeader.setText("");
 		rowHeader.setCellValueFactory(new PropertyValueFactory<SpreadSheetRowData, String>("rowHeader"));
-		
+
 		this.getColumns().add(rowHeader);
-		
+
 		// 行データの列 を作成する
-		for (int index = 0; index < columnSize; index++) {
+		for (int index = 0; index <= columnSize; index++) {
 			final String propertyKey = "" + index;
-			
+
 			TableColumn<SpreadSheetRowData, String> column = new TableColumn<SpreadSheetRowData, String>(propertyKey);
 			column.setText(ExcelUtil.createCelNumber(index));
 			column.setCellValueFactory(new Callback<CellDataFeatures<SpreadSheetRowData, String>, ObservableValue<String>>() {
-				
+
 				public ObservableValue<String> call(CellDataFeatures<SpreadSheetRowData, String> dataFeatures) {
 					CellProperty celProperty = dataFeatures.getValue().cellProperty(propertyKey);
-					
+
 					return celProperty.getCelData();
 				}
 			});
 			// 編集可能なセルを作成する
 			column.setCellFactory(new Callback<TableColumn<SpreadSheetRowData, String>, TableCell<SpreadSheetRowData, String>>() {
-				
+
 				@Override
 				public TableCell<SpreadSheetRowData, String> call(TableColumn<SpreadSheetRowData, String> arg0) {
-					return new TextFieldTableCell<SpreadSheetRowData, String>(new DefaultStringConverter());
+					TextFieldTableCell<SpreadSheetRowData, String> textFieldTableCell = new TextFieldTableCell<SpreadSheetRowData, String>();
+					textFieldTableCell.setConverter(new DefaultStringConverter());
+
+					return textFieldTableCell;
 				}
 			});
 			this.getColumns().add(column);
 		}
 	}
-	
+
 	/**
 	 * SpreadSheetView用の行データを作成します。
 	 *
@@ -100,34 +105,62 @@ public class SpreadSheetView extends TableView<SpreadSheetRowData> {
 	 * @param rowData
 	 * @return
 	 */
-	public Row createRow(int rowNumber, List<String> rowData) {
+	public Row createRow(int rowNumber, List<Map<String, Object>> rowData) {
 		Row row = sheet.createRow(rowNumber);
-		
+
+		int columnSize = this.getColumns().size();
+		for (int i = 0; i < columnSize; i++) {
+			row.createCell(i);
+		}
+
 		for (int cellIndex = 0; cellIndex < rowData.size(); cellIndex++) {
-			String data = rowData.get(cellIndex);
-			
-			Cell cell = row.createCell(cellIndex);
-			cell.setCellValue(data);
+			Map<String, Object> celData = rowData.get(cellIndex);
+
+			Cell cell = row.getCell(cellIndex);
+			int cellType = ((BigDecimal) celData.get("cellType")).intValue();
+			cell.setCellType(cellType);
+
+			String cellValue = (String) celData.get("cellValue");
+			switch (cellType) {
+				case Cell.CELL_TYPE_BLANK:
+					cell.setCellValue("");
+					break;
+				case Cell.CELL_TYPE_BOOLEAN:
+					cell.setCellValue(Boolean.parseBoolean(cellValue));
+					break;
+				case Cell.CELL_TYPE_NUMERIC:
+					cell.setCellValue(Double.parseDouble(cellValue));
+					break;
+				case Cell.CELL_TYPE_STRING:
+					cell.setCellValue(cellValue);
+					break;
+				case Cell.CELL_TYPE_FORMULA:
+					cell.setCellFormula(cellValue);
+					break;
+				case Cell.CELL_TYPE_ERROR:
+					cell.setCellValue("#ERROR");
+					break;
+			}
 		}
 		return row;
 	}
-	
+
 	/**
 	 * <スプレッドシート:1行のデータを表すクラス>
 	 *
 	 * @author honda
 	 */
 	public static class SpreadSheetRowData {
-		
+
 		public IntegerProperty rowHeader;
-		
+
 		public Row row;
-		
+
 		public SpreadSheetRowData(int rowNum, Row row) {
 			this.rowHeader = new SimpleIntegerProperty(rowNum + 1);
 			this.row = row;
 		}
-		
+
 		/**
 		 * 行番号を取得します。
 		 *
@@ -140,7 +173,7 @@ public class SpreadSheetView extends TableView<SpreadSheetRowData> {
 		public IntegerProperty rowHeaderProperty() {
 			return this.rowHeader;
 		}
-		
+
 		/**
 		 * 該当のカラム番号のデータを取得します
 		 *
@@ -148,15 +181,16 @@ public class SpreadSheetView extends TableView<SpreadSheetRowData> {
 		 * @return
 		 */
 		public CellProperty cellProperty(String columnNumber) {
-			Cell cellData = row.getCell(Integer.parseInt(columnNumber));
-			
-			if (cellData == null || cellData.getStringCellValue() == null) {
+			Cell cell = row.getCell(Integer.parseInt(columnNumber));
+
+			if (cell == null) {
 				return new CellProperty("");
 			}
-			return new CellProperty(cellData.getStringCellValue());
+			String cellValue = ExcelUtil.getCellValue(cell);
+			return new CellProperty(cellValue);
 		}
 	}
-	
+
 	/**
 	 * <スプレッドシート:1セルのデータを表すクラス>
 	 *
@@ -164,11 +198,11 @@ public class SpreadSheetView extends TableView<SpreadSheetRowData> {
 	 */
 	public static class CellProperty {
 		private StringProperty celData = new SimpleStringProperty();
-		
+
 		public CellProperty(String data) {
 			this.celData.set(data);
 		}
-		
+
 		public StringProperty getCelData() {
 			return celData;
 		}
--- client/src/main/java/fx/excel/upload/util/ExcelUtil.java
+++ client/src/main/java/fx/excel/upload/util/ExcelUtil.java
@@ -3,26 +3,30 @@ package fx.excel.upload.util;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Workbook;
 
 public class ExcelUtil {
-	
+
 	private static int ascii_alpha_start = 65;
-	
+
 	private static int alpha_amount = 26;
-	
+
 	public static String createCelNumber(int index) {
 		byte[] nums = null;
-		
+
 		if (index < 0) {
 			throw new IllegalArgumentException();
 		}
-		
+
 		for (int i = index;;) {
 			int divide = (i / alpha_amount), mod = (i % alpha_amount);
-			
+
 			byte asciiValue = new Integer(ascii_alpha_start + mod).byteValue();
 			nums = ArrayUtils.add(nums, asciiValue);
-			
+
 			if (i < alpha_amount) {
 				break;
 			}
@@ -30,5 +34,60 @@ public class ExcelUtil {
 		}
 		ArrayUtils.reverse(nums);
 		return new String(nums, StandardCharsets.US_ASCII);
+	}
+
+	/**
+	 * セルの値を文字列として返す
+	 *
+	 * @param cell
+	 * @return
+	 */
+	public static String getCellValue(Cell cell) {
+		switch (cell.getCellType()) {
+			case Cell.CELL_TYPE_BLANK:
+				// TODO 結合セルの場合どう扱うか？
+				return "";
+			case Cell.CELL_TYPE_BOOLEAN:
+				return Boolean.toString(cell.getBooleanCellValue());
+			case Cell.CELL_TYPE_NUMERIC:
+				return Double.toString(cell.getNumericCellValue());
+			case Cell.CELL_TYPE_STRING:
+				return cell.getStringCellValue();
+			case Cell.CELL_TYPE_FORMULA:
+				return getStringFormulaValue(cell);
+			case Cell.CELL_TYPE_ERROR:
+				return "#N/A";
+		}
+		return "";
+	}
+
+	/**
+	 * 数式を再計算し、文字列として返す
+	 *
+	 * <p>
+	 * ◇使用可能な関数について
+	 * </p>
+	 * <ul>
+	 * <li>http://poi.apache.org/spreadsheet/eval-devguide.html</li>
+	 * <li>{@link org.apache.poi.ss.formula.eval.FunctionEval}</li>
+	 * </ul>
+	 *
+	 * @param cell
+	 * @return
+	 */
+	public static String getStringFormulaValue(Cell cell) {
+		try {
+			Workbook book = cell.getSheet().getWorkbook();
+
+			CreationHelper helper = book.getCreationHelper();
+			FormulaEvaluator evaluator = helper.createFormulaEvaluator();
+			Cell evalCell = evaluator.evaluateInCell(cell);
+
+			return getCellValue(evalCell);
+		} catch (Exception e) {
+			System.err.println(cell.getCellFormula());
+			e.printStackTrace();
+			return "#UNSUPPORT";
+		}
 	}
 }
